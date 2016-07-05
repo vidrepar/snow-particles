d6c88b509c541ebc64c67bdbf7b892a776078b9d
vidrepar <vid.repar@gmail.com>
2016-07-05 10:22:49 +0200

    microWind and global wind + some other fixes

--- js/Snow.js
+++ js/Snow.js
@@ -10,24 +10,31 @@
  * @param container
  * @constructor
  */
-var Snow = function (container) {
+var Snow = function (container, wind) {
 
     /*
     * container is like a movieclip
     * */
     this.container = container;
 
+    this.wind = wind;
+
     /*
     * adding depth
     * */
     this.distance = Math.random()+0.3;
+    this.randomRotation = Math.random();
+
+    this.microWind = {
+        force:Math.random()*0.5
+    };
 
     /*
     * we won't call _draw from app.js
     * that's why there's _
     * */
     this._draw();
-    this._startWind();
+    this._randomWind();
 
     createjs.Ticker.addEventListener('tick', this._onAnimation.bind(this));
     /*
@@ -39,19 +46,6 @@ var Snow = function (container) {
 
 };
 
-Snow.prototype._startWind = function () {
-
-    var self = this;
-
-    setInterval(function () {
-
-        var currentX = self.particle.x;
-        TweenLite.to(self.particle, 4, { x: currentX+Math.random()*100-50, ease:Sine.easeInOut })
-
-    }, 4000);
-
-};
-
 Snow.prototype._draw = function () {
 
     /*
@@ -60,13 +54,19 @@ Snow.prototype._draw = function () {
     * */
 
     this.particle = new createjs.Container();
+    this.particleContainer = new createjs.Container();
 
     this.shape = new createjs.Shape();
     this.shape.graphics.beginFill('#000000');
     this.shape.graphics.drawCircle(0,0,5,20);
     this.shape.graphics.endFill();
 
+    this.particle.regX = 7;
+    this.particle.rotation = Math.random()*20;
+
     this.particle.scaleX = this.particle.scaleY = this.distance+0.3;
+
+    this.particleContainer.addChild(this.particle);
 
     this.particle.addChild(this.shape);
 
@@ -74,17 +74,36 @@ Snow.prototype._draw = function () {
 
 };
 
-Snow.prototype.destroy = function () {
+Snow.prototype._randomWind = function () {
 
-    
+    var nWind = Math.random()-0.5;
+
+    TweenLite.to(this.microWind, Math.random()*2+1,
+        {
+            force:nWind,
+            delay:Math.random()+1,
+            onComplete:this._randomWind.bind(this)
+        });
 
 };
 
-Snow.prototype.positionCheck = function () {
+Snow.prototype._positionCheck = function () {
 
-    if(this.particle.y > this.container.canvas.clientHeight){
+    if(this.particle.y > this.container.canvas.clientHeight+10){
 
         this._reset();
+
+    }
+
+    if(this.particle.x > this.container.canvas.clientWidth+10){
+
+        this.particle.x = -10;
+
+    }
+
+    if(this.particle.x < -10){
+
+        this.particle.x = this.container.canvas.clientWidth+10;
 
     }
 
@@ -100,8 +119,14 @@ Snow.prototype._reset = function () {
 
 Snow.prototype._onAnimation = function () {
 
-    this.particle.y += (1+this.distance);
+    /*
+    * gravity
+    * */
+    this.particle.y += (1+this.distance)-this.wind.force/2;
+    this.particle.x += (this.wind.force*10)*this.distance+this.microWind.force;
 
-    this.positionCheck();
+    this.particle.rotation += this.wind.force*this.randomRotation*20;
+
+    this._positionCheck();
 
 };
--- js/app.js
+++ js/app.js
@@ -2,20 +2,40 @@ var app = {
 
     NUM_PARTICLES:200,
 
-    particleArray:[],
-    stage:null,
+    particleArray   :[],
+    stage           :null,
+    wind            :{
+        max:1,
+        min:0,
+        direction:'both',
+        force:Math.random()-0.5
+    },
 
     init: function () {
 
         app.setupStage();
         app.setupTicker();
         app.createParticles();
+        app.randomWind();
 
     },
+
+    randomWind:function () {
+
+        var nWind = Math.random()-0.5;
+
+        TweenLite.to(app.wind, Math.random()*3+2,
+            {
+                force:nWind,
+                delay:Math.random()+1,
+                onComplete:app.randomWind
+            });
+
+    },
+
     setupStage: function () {
 
         var $canvas = $('#snow-canvas');
-
         app.stage = new createjs.Stage($canvas[0]);
 
     },
@@ -38,17 +58,17 @@ var app = {
 
         while(app.NUM_PARTICLES--){
 
-            var snow = new Snow(app.stage);
+            var snow = new Snow(app.stage, app.wind);
 
-            snow.particle.x = Math.random()*1200;
-            snow.particle.y = Math.random()*500;
+            snow.particle.x = Math.random()*1000;
+            snow.particle.y = Math.random()*600-50;
 
             app.particleArray.push(snow);
 
         }
 
     }
-    
+
 };
 
 app.init();
